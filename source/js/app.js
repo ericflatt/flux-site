099bc39f43cec151a5c46466605a2f7dc80c110f
Eric Flatt <eflatt18@gmail.com>
2019-08-16 21:01:23 -0400

    added navigation

--- source/js/app.js
+++ source/js/app.js
@@ -9,3 +9,65 @@ window.sr = ScrollReveal();
 //     origin: 'bottom',
 //     distance: '100px' 
 // });
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+//transparent to solid color nav bar
+$(document).ready(function() {
+     
+    $(window).scroll(function() {
+
+      if($(this).scrollTop() > 500) { 
+          $('.navbar').addClass('solid');
+      } else {
+          $('.navbar').removeClass('solid');
+      }
+    });
+});
+
+
+var modal = document.getElementById("modal");
+
+var btn = document.getElementById("btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function anchorClick() {
+    document.getElementById("modal").style.display = "none";
+  }
