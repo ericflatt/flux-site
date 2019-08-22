32819bb14cf424ee87860fa2284cf72414a0282e
Eric Flatt <eflatt18@gmail.com>
2019-08-21 23:18:18 -0400

    need to figure out how to potentially shuffle order of portfolio cards

--- source/js/app.js
+++ source/js/app.js
@@ -69,3 +69,12 @@ window.onclick = function(event) {
 function anchorClick() {
     document.getElementById("modal").style.display = "none";
   }
+
+//randomize cards
+//$(function () {
+//  var parent = $("#shuffle");
+//  var divs = parent.children();
+//  while (divs.length) {
+//      parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+//  }
+//});
