dbb2e3823b44bf2c3581186937dc7733c3d99be2
Eric Flatt <eflatt18@gmail.com>
2019-09-03 19:15:33 -0400

    nav fixes

--- source/js/app.js
+++ source/js/app.js
@@ -38,9 +38,9 @@ $(document).ready(function() {
     $(window).scroll(function() {
 
       if($(this).scrollTop() > 500) { 
-          $('.navbar').addClass('solid');
+          $('.navContent').addClass('solid');
       } else {
-          $('.navbar').removeClass('solid');
+          $('.navContent').removeClass('solid');
       }
     });
 });
