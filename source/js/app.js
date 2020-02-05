cbcca5d4fd224abfce0944fa6bc1f2fd5eab4c55
Eric Flatt <eflatt18@gmail.com>
2020-02-05 16:35:25 -0500

    doesnt work

--- source/js/app.js
+++ source/js/app.js
@@ -141,3 +141,22 @@ function portfolioViewMore() {
   $(".person").addClass("card-display-shown");
   console.log("view more clicked")
 }
+
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
+
+var keyHandler = function (event) {
+
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	current++;
+
+	if (pattern.length === current) {
+		current = 0;
+		window.location.href = "http://bluuuuuuux.com/";
+	}
+
+};
