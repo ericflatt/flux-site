c2f4067186d277518a84a6900dc5a1ffbcf4d41d
Varun Khatri <VarunMK05@gmail.com>
2020-05-03 19:51:21 -0400

    initial addition of fluxathon winner
    fluxathon winners draft 1

--- source/js/app.js
+++ source/js/app.js
@@ -160,3 +160,32 @@ var keyHandler = function (event) {
 	}
 
 };
+
+
+// Slider Controls
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider-object");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.animationName = "fadeIn";
+  slides[slideIndex-1].style.display = "grid";
+}
